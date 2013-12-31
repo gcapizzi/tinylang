cab8a8266fc7100f2dfff5bee7b2544737ac89f1
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-12-31 18:41:18 +0100

    Specs refactoring

--- spec/lib/tinylang/vm/parser_spec.rb
+++ spec/lib/tinylang/vm/parser_spec.rb
@@ -4,26 +4,26 @@ require 'tinylang'
 
 describe Tiny::Parser do
   it 'parses integer literals' do
-    expect(subject.parse('  123  ')).to eq(program(integer('123')))
+    expect(subject.parse('  123  ')).to eq(program(int('123')))
   end
 
   it 'parses method calls' do
-    parsed_method_call = subject.parse('2.method().other_method()')
-    expect(parsed_method_call).to eq(program(method_call(integer('2'), 'method', 'other_method')))
+    parsed_program = subject.parse('2.method().other_method()')
+    expect(parsed_program).to eq(program(method_call(int('2'), 'method', 'other_method')))
 
-    expect(subject.parse('x.some_method()')).to eq(program(method_call(variable('x'), 'some_method')))
+    expect(subject.parse('x.some_method()')).to eq(program(method_call(var('x'), 'some_method')))
   end
 
-  it 'parses expression lists' do
-    expect(subject.parse("123\n456")).to eq(program(integer('123'), integer('456')))
+  it 'parses multi-line programs' do
+    expect(subject.parse("123\n456")).to eq(program(int('123'), int('456')))
   end
 
   it 'parses assignments' do
-    expect(subject.parse('x = 42')).to eq(program(assignment('x', integer('42'))))
+    expect(subject.parse('x = 42')).to eq(program(assignment('x', int('42'))))
   end
 
   it 'parses variables' do
-    expect(subject.parse('   x   ')).to eq(program(variable('x')))
+    expect(subject.parse('   x   ')).to eq(program(var('x')))
   end
 
   private
@@ -32,16 +32,20 @@ describe Tiny::Parser do
     { program: expressions }
   end
 
-  def integer(integer)
+  def int(integer)
     { integer: integer }
   end
 
-  def variable(variable)
+  def var(variable)
     { variable: variable }
   end
 
-  def method_call(object, *method_chain)
-    { method_call: { object: object, method_chain: method_chain.map { |method| { method: method } } } }
+  def method_chain(methods)
+    methods.map { |method| { method: method } }
+  end
+
+  def method_call(object, *methods)
+    { method_call: { object: object, method_chain: method_chain(methods) } }
   end
 
   def assignment(variable, value)
